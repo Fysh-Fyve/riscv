d0cbc8e485d7a35ff4938b6a781a27544fdb48ba
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2024-04-08 15:22:40 -0600

    Update ROM

--- rtl/core/rom_pkg.vhd
+++ rtl/core/rom_pkg.vhd
@@ -10,12 +10,12 @@ use work.fysh_fyve.all;
 package rom is
   constant rom_arr : mem_t (0 to ROM_NUM_WORDS-1) := (
     x"37210100",
-    x"ef00801e",
+    x"ef00001a",
     x"63000000",
     x"6b050000",
     x"67800000",
     x"b7050000",
-    x"83a58538",
+    x"83a5c536",
     x"83a50500",
     x"33d5a500",
     x"13751500",
@@ -25,31 +25,31 @@ package rom is
     x"23248100",
     x"23229100",
     x"b7040000",
-    x"03a5c438",
+    x"03a50437",
     x"b7150100",
     x"1384d5dc",
     x"93050400",
     x"ef00c00e",
     x"33058500",
-    x"23a6a438",
+    x"23a8a436",
     x"8320c100",
     x"03248100",
     x"83244100",
     x"13010101",
     x"67800000",
     x"b7050000",
-    x"23a6a538",
+    x"23a8a536",
     x"67800000",
     x"37050000",
-    x"03258538",
+    x"0325c536",
     x"03250500",
     x"67800000",
     x"b7050000",
-    x"83a58538",
+    x"83a5c536",
     x"23a0a500",
     x"67800000",
     x"37060000",
-    x"03264638",
+    x"03268636",
     x"83260600",
     x"13071000",
     x"3315a700",
@@ -62,7 +62,7 @@ package rom is
     x"2320a600",
     x"67800000",
     x"37060000",
-    x"03268638",
+    x"0326c636",
     x"83260600",
     x"13071000",
     x"3315a700",
@@ -75,14 +75,14 @@ package rom is
     x"2320a600",
     x"67800000",
     x"37050000",
-    x"03250538",
+    x"03254536",
     x"03250500",
     x"67800000",
     x"9305f000",
     x"63eea500",
     x"13152500",
     x"b7050000",
-    x"9385c52d",
+    x"9385052d",
     x"3385a500",
     x"03250500",
     x"67800000",
@@ -102,96 +102,93 @@ package rom is
     x"130101ff",
     x"23261100",
     x"23248100",
-    x"23229100",
-    x"13840500",
-    x"93040500",
-    x"eff0dfef",
-    x"137505f0",
-    x"93943400",
-    x"33649400",
-    x"3365a400",
-    x"13640508",
+    x"1315d501",
+    x"1355a501",
+    x"93f57500",
+    x"33e5a500",
+    x"13040508",
     x"13050400",
-    x"eff01fef",
+    x"eff01ff0",
     x"13050400",
     x"8320c100",
     x"03248100",
-    x"83244100",
     x"13010101",
     x"67800000",
-    x"b7050000",
-    x"03a68539",
-    x"6340a602",
-    x"37050000",
-    x"83260539",
-    x"13060000",
-    x"93861600",
-    x"93f67600",
-    x"2328d538",
-    x"23ac0538",
-    x"13051600",
-    x"23aca538",
-    x"67800000",
-    x"130101ff",
-    x"23261100",
-    x"23248100",
-    x"23229100",
+    x"130101fe",
+    x"232e1100",
+    x"232c8100",
+    x"232a9100",
+    x"23282101",
+    x"23263101",
     x"13058001",
     x"93050000",
-    x"eff09fe9",
+    x"eff09fed",
     x"13059001",
     x"93050000",
-    x"eff0dfe8",
+    x"eff0dfec",
     x"1305a001",
     x"93050000",
-    x"eff01fe8",
+    x"eff01fec",
     x"1305b001",
     x"93050000",
-    x"eff05fe7",
+    x"eff05feb",
     x"37040000",
-    x"23280438",
+    x"232a0436",
     x"b7040000",
-    x"23aa0438",
-    x"37050000",
-    x"232c0538",
-    x"03250439",
-    x"83a54439",
-    x"eff0dff1",
-    x"83a54439",
-    x"03250439",
+    x"23ac0436",
+    x"37090000",
+    x"232e0936",
+    x"b7090000",
+    x"13050040",
+    x"23a0a938",
+    x"6f00c000",
+    x"13051500",
+    x"232ea936",
+    x"03254437",
+    x"83a58437",
+    x"eff05ff4",
+    x"83a58437",
+    x"03254437",
     x"93852500",
-    x"eff0dff0",
-    x"83a54439",
-    x"03250439",
+    x"eff05ff3",
+    x"83a58437",
+    x"03254437",
     x"93853500",
-    x"eff0dfef",
-    x"03250439",
-    x"83a54439",
+    x"eff05ff2",
+    x"03254437",
+    x"83a58437",
     x"13051500",
     x"93851500",
-    x"eff09fee",
-    x"03250439",
-    x"83a54439",
+    x"eff01ff1",
+    x"03254437",
+    x"83a58437",
     x"13051500",
     x"93854500",
-    x"eff05fed",
-    x"03250439",
-    x"83a54439",
+    x"eff0dfef",
+    x"03254437",
+    x"83a58437",
     x"13052500",
-    x"eff05fec",
-    x"03250439",
-    x"83a54439",
+    x"eff0dfee",
+    x"03254437",
+    x"83a58437",
     x"13052500",
     x"93852500",
-    x"eff01feb",
-    x"03250439",
-    x"83a54439",
+    x"eff09fed",
+    x"03254437",
+    x"83a58437",
     x"13052500",
     x"93853500",
-    x"eff0dfe9",
-    x"13050040",
-    x"eff05fee",
-    x"6ff0dff6",
+    x"eff05fec",
+    x"0325c937",
+    x"83a50938",
+    x"e342b5f6",
+    x"83254437",
+    x"13050000",
+    x"93851500",
+    x"93f57500",
+    x"232ab436",
+    x"232e0936",
+    x"6ff09ff4",
     x"3f000000",
     x"06000000",
     x"5b000000",
@@ -215,23 +212,19 @@ package rom is
     x"1b0c0200",
     x"18000000",
     x"18000000",
-    x"30feffff",
-    x"50000000",
+    x"3cfeffff",
+    x"3c000000",
     x"00440e10",
-    x"4c810188",
-    x"02890300",
-    x"10000000",
+    x"48810188",
+    x"02000000",
+    x"1c000000",
     x"34000000",
-    x"64feffff",
-    x"34000000",
-    x"00000000",
-    x"18000000",
-    x"48000000",
-    x"84feffff",
-    x"f0000000",
-    x"00440e10",
-    x"4c810188",
-    x"02890300",
+    x"5cfeffff",
+    x"2c010000",
+    x"00440e20",
+    x"54810188",
+    x"02890392",
+    x"04930500",
     x"00000000",
     x"e4beadde",
     x"e8beadde",
@@ -254,53 +247,22 @@ end package rom;
 --! 	.cfi_def_cfa_offset 16
 --! 	sw	ra, 12(sp)                      # 4-byte Folded Spill
 --! 	sw	s0, 8(sp)                       # 4-byte Folded Spill
---! 	sw	s1, 4(sp)                       # 4-byte Folded Spill
 --! 	.cfi_offset ra, -4
 --! 	.cfi_offset s0, -8
---! 	.cfi_offset s1, -12
---! 	mv	s0, a1
---! 	mv	s1, a0
---! 	call	fysh_gpio_read_all
---! 	andi	a0, a0, -256
---! 	slli	s1, s1, 3
---! 	or	s0, s0, s1
---! 	or	a0, s0, a0
---! 	ori	s0, a0, 128
+--! 	slli	a0, a0, 29
+--! 	srli	a0, a0, 26
+--! 	andi	a1, a1, 7
+--! 	or	a0, a1, a0
+--! 	addi	s0, a0, 128
 --! 	mv	a0, s0
 --! 	call	fysh_gpio_write_all
 --! 	mv	a0, s0
 --! 	lw	ra, 12(sp)                      # 4-byte Folded Reload
 --! 	lw	s0, 8(sp)                       # 4-byte Folded Reload
---! 	lw	s1, 4(sp)                       # 4-byte Folded Reload
 --! 	addi	sp, sp, 16
 --! 	ret
 --! .Lfunc_end0:
 --! 	.size	pixel, .Lfunc_end0-pixel
---! 	.cfi_endproc
---!                                         # -- End function
---! 	.globl	wait_increment                  # -- Begin function wait_increment
---! 	.p2align	2
---! 	.type	wait_increment,@function
---! wait_increment:                         # @wait_increment
---! 	.cfi_startproc
---! # %bb.0:                                # %entry
---! 	lui	a1, %hi(.Lcount)
---! 	lw	a2, %lo(.Lcount)(a1)
---! 	blt	a2, a0, .LBB1_2
---! # %bb.1:                                # %then
---! 	lui	a0, %hi(.Lx)
---! 	lw	a3, %lo(.Lx)(a0)
---! 	li	a2, 0
---! 	addi	a3, a3, 1
---! 	andi	a3, a3, 7
---! 	sw	a3, %lo(.Lx)(a0)
---! 	sw	zero, %lo(.Lcount)(a1)
---! .LBB1_2:                                # %if_exit
---! 	addi	a0, a2, 1
---! 	sw	a0, %lo(.Lcount)(a1)
---! 	ret
---! .Lfunc_end1:
---! 	.size	wait_increment, .Lfunc_end1-wait_increment
 --! 	.cfi_endproc
 --!                                         # -- End function
 --! 	.globl	main                            # -- Begin function main
@@ -309,14 +271,18 @@ end package rom;
 --! main:                                   # @main
 --! 	.cfi_startproc
 --! # %bb.0:                                # %entry
---! 	addi	sp, sp, -16
---! 	.cfi_def_cfa_offset 16
---! 	sw	ra, 12(sp)                      # 4-byte Folded Spill
---! 	sw	s0, 8(sp)                       # 4-byte Folded Spill
---! 	sw	s1, 4(sp)                       # 4-byte Folded Spill
+--! 	addi	sp, sp, -32
+--! 	.cfi_def_cfa_offset 32
+--! 	sw	ra, 28(sp)                      # 4-byte Folded Spill
+--! 	sw	s0, 24(sp)                      # 4-byte Folded Spill
+--! 	sw	s1, 20(sp)                      # 4-byte Folded Spill
+--! 	sw	s2, 16(sp)                      # 4-byte Folded Spill
+--! 	sw	s3, 12(sp)                      # 4-byte Folded Spill
 --! 	.cfi_offset ra, -4
 --! 	.cfi_offset s0, -8
 --! 	.cfi_offset s1, -12
+--! 	.cfi_offset s2, -16
+--! 	.cfi_offset s3, -20
 --! 	li	a0, 24
 --! 	li	a1, 0
 --! 	call	pin_mode
@@ -333,9 +299,17 @@ end package rom;
 --! 	sw	zero, %lo(.Lx)(s0)
 --! 	lui	s1, %hi(.Ly)
 --! 	sw	zero, %lo(.Ly)(s1)
---! 	lui	a0, %hi(.Lcount)
---! 	sw	zero, %lo(.Lcount)(a0)
---! .LBB2_1:                                # %loop_cond
+--! 	lui	s2, %hi(.Lcount)
+--! 	sw	zero, %lo(.Lcount)(s2)
+--! 	lui	s3, %hi(.Lcount_lim)
+--! 	li	a0, 1024
+--! 	sw	a0, %lo(.Lcount_lim)(s3)
+--! 	j	.LBB1_2
+--! .LBB1_1:                                # %if_exit
+--!                                         #   in Loop: Header=BB1_2 Depth=1
+--! 	addi	a0, a0, 1
+--! 	sw	a0, %lo(.Lcount)(s2)
+--! .LBB1_2:                                # %loop_cond
 --!                                         # =>This Inner Loop Header: Depth=1
 --! 	lw	a0, %lo(.Lx)(s0)
 --! 	lw	a1, %lo(.Ly)(s1)
@@ -372,11 +346,20 @@ end package rom;
 --! 	addi	a0, a0, 2
 --! 	addi	a1, a1, 3
 --! 	call	pixel
---! 	li	a0, 1024
---! 	call	wait_increment
---! 	j	.LBB2_1
---! .Lfunc_end2:
---! 	.size	main, .Lfunc_end2-main
+--! 	lw	a0, %lo(.Lcount)(s2)
+--! 	lw	a1, %lo(.Lcount_lim)(s3)
+--! 	blt	a0, a1, .LBB1_1
+--! # %bb.3:                                # %then
+--!                                         #   in Loop: Header=BB1_2 Depth=1
+--! 	lw	a1, %lo(.Lx)(s0)
+--! 	li	a0, 0
+--! 	addi	a1, a1, 1
+--! 	andi	a1, a1, 7
+--! 	sw	a1, %lo(.Lx)(s0)
+--! 	sw	zero, %lo(.Lcount)(s2)
+--! 	j	.LBB1_1
+--! .Lfunc_end1:
+--! 	.size	main, .Lfunc_end1-main
 --! 	.cfi_endproc
 --!                                         # -- End function
 --! 	.type	.Lx,@object                     # @x
@@ -397,6 +380,12 @@ end package rom;
 --! .Lcount:
 --! 	.word	0                               # 0x0
 --! 	.size	.Lcount, 4
+--! 
+--! 	.type	.Lcount_lim,@object             # @count_lim
+--! 	.p2align	2, 0x0
+--! .Lcount_lim:
+--! 	.word	0                               # 0x0
+--! 	.size	.Lcount_lim, 4
 --! 
 --! 	.section	".note.GNU-stack","",@progbits
 --! ></*>
@@ -436,14 +425,9 @@ end package rom;
 --! 
 --! >(pixel) ><row> ><col>
 --! ><>
---!    o+) ><gpio> ~
---!    (+o (><gpio> &
---!          (<°}>< << ><{{{°>) ></*> keep everything in GPIO
---!                                   the same but the first 7 bits <*/><
---!         ) |
---!         (><{°> << ><{{{°>) | ><//> Output enable
---!         (><row> << ><{{°>) | ><//> Row is next higher 3 bits
---!         ><col> ~ ><//> Col is first 3 bits
+--!    (+o  (><{°> << ><{{{°>) | ><//> Output enable
+--!         ((><row> & ><{{{°>) << ><{{°>) | ><//> Row is next higher 3 bits
+--!         (><col> & ><{{{°>) ~ ><//> Col is first 3 bits
 --! <><
 --! 
 --! ><//> It's little endian somehow so we gotta start at 24
@@ -457,17 +441,7 @@ end package rom;
 --! ><x> = ><(°> ~
 --! ><y> = ><(°> ~
 --! ><count> = ><(°> ~
---! 
---! >(wait_increment) ><count_lim>
---! ><>
---!    ><(((^> [><count> o≈ ><count_lim>]
---!    ><>
---!       ><x> = (><x> ><{°>) & ><{{{°> ~ ><//> x = (x + 1) & 7 (reset to 0 if 8)
---!       ><count> = ><(°> ~
---!    <><
---!    >><count> ~
---! 
---! <><
+--! ><count_lim> = ><{((((((((((°> ~
 --! 
 --! ><(((@> [><{°>] ><//> 1
 --! ><>
@@ -479,7 +453,14 @@ end package rom;
 --!    [><x> ><{(°> >(pixel)  ><y>        ] ~ ><//> 2 0
 --!    [><x> ><{(°> >(pixel)  ><y> ><{(°> ] ~ ><//> 2 2
 --!    [><x> ><{(°> >(pixel)  ><y> ><{{°> ] ~ ><//> 2 3
---!    [>(wait_increment) ><{((((((((((°> ] ~ ><//> 1024
+--!    ><//> [>(wait increment) ><{((((((((((°> ] ~ ><//> 1024
+--! 
+--!    ><(((^> [><count> o≈ ><count_lim>]
+--!    ><>
+--!       ><x> = (><x> ><{°>) & ><{{{°> ~ ><//> x = (x + 1) & 7 (reset to 0 if 8)
+--!       ><count> = ><(°> ~
+--!    <><
+--!    >><count> ~
 --! <><
 --! 
 --! 
